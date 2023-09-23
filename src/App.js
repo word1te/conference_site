0b557abf72f8f5e17954966da4ac3c4f035c31a6
ItsZeroFour <ItsZeroFourX@gmail.com>
2023-09-23 18:25:13 +0300

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import Footer from "Components/Footer/Footer";
+import Header from "Components/Header/Header";
+import Section1 from "Components/Section-1/Section-1";
+import Section2 from "Components/Section-2/Section-2";
+import Section3 from "Components/Section-3/Section-3";
+import React from "react";
 
 function App() {
   return (
     <React.Fragment>
-      
+      <div className="container">
+        <Header />
+          <main>
+            <Section1/>
+            <Section2/>
+            <Section3/>
+          </main>
+        <Footer />
+      </div>
     </React.Fragment>
   );
 }
--- src/Components/Section-2/Section-2.jsx
+++ src/Components/Section-2/Section-2.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import style from './Section-2.module.scss'
+import React from "react";
+import style from "./Section-2.module.scss";
+import blockHuman from "../../images/Section2/Daniil.png";
 
 const Section2 = () => {
   return (
-    <section className={style.section}></section>
-  )
-}
+    <section className={style.section}>
+      <h2>Даниил Андреев</h2>
 
-export default Section2
+      <div className={style.section__left}>
+        <form>
+          <input type="text" />
+          <input type="password" />
+          <button type="submit">Отправить</button>
+        </form>
+
+        <div className={style.section__about}>
+          <p>
+            Я работаю сразу на 3-х направлениях. Являюсь как Вэб дизайнером, так
+            и Frontend и backend (fullstack) разработчиков. Без вэб дизайна
+            сайты были бы как википедии, без фронтенда небыло сайтов вовсе, а
+            без бэкэнда небыло бы возможности Писать сообщения на почту через
+            форму на сайте, небыло бы регистрации, видео на ютубе и т.д
+          </p>
+        </div>
+      </div>
+
+      <div className={style.section__right}>
+        <img src={blockHuman} alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default Section2;
