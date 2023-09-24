928cd195e074d21aa86e1defc78123678ea26d98
ItsZeroFour <ItsZeroFourX@gmail.com>
2023-09-24 10:22:35 +0300

    create section 2

--- src/App.js
+++ src/App.js
@@ -4,24 +4,33 @@ import FirstScreen from "./Components/FirstScreen/FirstScreen";
 import Section1 from "./Components/Section-1/Section-1";
 import Section2 from "./Components/Section-2/Section-2";
 import Section3 from "./Components/Section-3/Section-3";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 function App() {
+  const [elementInView, setElementInView] = useState(false)
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (inView) setElementInView(true)
+  }, [inView])
+
   return (
     <>
       <div className="container">
         <Header />
-          <main>
-            <FirstScreen />
-            <Section1/>
-            {/*<Section2/>*/}
-            <Section3/>
-          </main>
+        <main>
+          <FirstScreen />
+          <Section1 />
+          <Section2 userRef={ref} elementInView={elementInView} />
+          <Section3 />
+        </main>
         <Footer />
       </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/Components/FirstScreen/FirstScreen.jsx
+++ src/Components/FirstScreen/FirstScreen.jsx
@@ -1,42 +1,44 @@
-import React from 'react'
-import style from './FirstScreen.module.scss'
-import arrow from '../../images/arrow.png'
-import desktop from '../../images/desktop.png'
+import React from "react";
+import style from "./FirstScreen.module.scss";
+import arrow from "../../images/arrow.png";
+import desktop from "../../images/desktop.png";
 
 const FirstScreen = () => {
+  return (
+    <section className={style.section}>
+      <div>
+        <div className={style.title}>Frontend Backend</div>
 
-    return (
-        <section className={style.section}>
-            <div>
-                <div className={style.title}>Frontend Backend</div>
+        <div className={style.keywords}>
+          <div className={style.arrow}>
+            <img src={arrow} alt="arrow" />
+          </div>
+          <div className={style.keyword}>ECMAScript 6</div>
+          <div className={style.keyword}>Веб специальности</div>
+          <div className={style.keyword}>Самый дорогой сайт</div>
+          <div className={style.keyword}>Команда</div>
+        </div>
 
-                <div className={style.keywords}>
-                    <div className={style.arrow}>
-                        <img src={arrow} alt="arrow"/>
-                    </div>
-                    <div className={style.keyword}>ECMAScript 6</div>
-                    <div className={style.keyword}>Веб специальности</div>
-                    <div className={style.keyword}>Самый дорогой сайт</div>
-                    <div className={style.keyword}>Команда</div>
-                </div>
+        <p className={style.tagline}>
+          C чего начинается эра JavaScript, 3 направления в Web разработке,
+          самый дорогой сайт в мире и почему хорошие сайты создаются в команде
+        </p>
 
-                <p className={style.tagline}>
-                    C чего начинается эра JavaScript, 3 направления в Web разработке, самый дорогой сайт в мире и почему хорошие сайты создаются в команде
-                </p>
+        <div className={style.nav_arrow}>
+          <div className={style.nav_arrow_bg}>
+            <img src={arrow} alt="arrow" />
+          </div>
+          <p className={style.nav_arrow_text}>
+            ECMAScript <span className={style.nav_arrow_yellow}>6</span>
+          </p>
+        </div>
 
-                <div className={style.nav_arrow}>
-                    <div className={style.nav_arrow_bg}>
-                        <img src={arrow} alt="arrow"/>
-                    </div>
-                    <p className={style.nav_arrow_text}>ECMAScript <span className={style.nav_arrow_yellow}>6</span></p>
-                </div>
-
-                <div className={style.desktop}>
-                    <img src={desktop} alt="desktop"/>
-                </div>
-            </div>
-        </section>
-    );
+        <div className={style.desktop}>
+          <img src={desktop} alt="desktop" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FirstScreen;
